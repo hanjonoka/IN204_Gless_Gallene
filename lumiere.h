3901b1d0173d11b7f392da067e498174b0833194
bastiengless <bastien.gless@ensta-paris.fr>
2021-12-07 11:06:02 +0100

    objets and lumiere

--- lumiere.h
+++ lumiere.h
@@ -2,35 +2,44 @@
 #include "objets.h"
 #include <vector>
 
-class Rayon:
+class Rayon_t:
 {
-private:
+public:
     Color_t color;
     Vector_t origine;
-    Vector_unit_t direction;
-public:
-    Intersection_t trouve_premier_obstacle(std::vector<Sphere> scene)
+    Vector_t direction;
+
+    Rayon_t() : color(Color_t()), origine(Vector_t()), direction(Vector_unit_t())
+    {}
+
+    Rayon_t(Color_t color, Vector_t origine, Vector_unit_t direction) :
+        color(color), origine(origine), direction(direction)
+    {}
+
+
+
+    Intersection_t trouve_premier_obstacle(std::vector<Sphere_t> scene)
     {
         // Trouver un obstacle de la scene qui intersecte, donc dont l'intersection n'est pas NULL, pour avoir une distance
-        Intersect_t premier_obstacle = NULL;
+        Intersection_t* premier_obstacle = NULL;
         //scene[0].intersect
         //double distance_min = scene[0].intersect(this).
 
         // Iterer dans la scene pour trouver l'objet dont l'intersection est la plus proche de l'origine du rayon
-        for(auto it = std::begin(v); it != std::end(v); ++it) {
-            Intersection_t inter = *it.intersect(this);
+        for(auto it = std::begin(scene); it != std::end(scene); ++it) {
+            Intersection_t* inter = it.intersect(*this);
             if (inter != NULL) {
-                if (premier_obstacle == NULL) {premier_obstacle = inter};
+                if (premier_obstacle == NULL) {premier_obstacle = inter;}
                 else {
-                    if (premier_obstacle.distance > inter.distance) {premier_obstacle = inter;}
+                    if (premier_obstacle->distance > inter->distance) {premier_obstacle = inter;}
                 }
             }
         }
-        return premier_obstacle;
+        return *premier_obstacle;
     }
     void lancer_rayon(Intersection_t inter)
     {
-        Rayon diff = Rayon()
+        Rayon_t diff = Rayon_t()
     }
 
 }
--- objets.h
+++ objets.h
@@ -1,7 +1,7 @@
 #include "utils.h"
 //#include "rayons.h"
 
-class Sphere:
+class Sphere_t:
 {   
 private:    
     Vector_t centre;
@@ -9,34 +9,48 @@ private:
     Color_t couleur;
 
 public:
+    Sphere_t() : centre(Vector_t()), radius(1), couleur(Color_t(0, 0, 0))
+    {}
+
+    Sphere_t(Vector_t centre, double radius, Color_t couleur) :
+        centre(centre), radius(radius), couleur(couleur)
+    {}
+
+    Sphere_t(const Sphere_t &sphere)
+    {
+        centre = sphere.centre;
+        radius = sphere.radius;
+        couleur = sphere.couleur;
+    }
+
     // using https://www.lighthouse3d.com/tutorials/maths/ray-sphere-intersection/
-    Intersection_t intersect(Rayon ray) // Return an object Intersection representing the intersection of the ray with the current object
+    Intersection_t intersect(Rayon_t ray) // Return an object Intersection representing the intersection of the ray with the current object
     {
         // check if pc is not behind the origin of the ray
-        Vector_t v = this.centre - ray.origin;  // %%%% VERIFIER QUE LE - FONCTIONNE SUR LES POSITIONS
-        double distance = v*ray.direction;
+        Vector_t v = this->centre - ray.origine;  // %%%% VERIFIER QUE LE - FONCTIONNE SUR LES POSITIONS
+        double distance = v ^ ray.direction;
         if (distance <= 0) {return NULL;}  // %%%% VERIFIER QUE LE PRODUIT SCALAIRE * FONCTIONNE
 
         // find pc the projection of the center on the ray
-        Vector_t pc = ray.origin + ray.direction * distance; // %%% VERIF QUE DOUBLE * VECTOR FONCTIONNE OU IMPLEMENTER PROJ
+        Vector_t pc = ray.origine + ray.direction * distance; // %%% VERIF QUE DOUBLE * VECTOR FONCTIONNE OU IMPLEMENTER PROJ
 
         // check the distance d between pc and the center
-        double d = norm(pc - this.center);
+        double d = (pc - this->centre).norme();
         // if d > r then no intersection
-        if (d > this.radius) {return NULL;}
+        if (d > this->radius) {return NULL;}
         // if d == r then intersection = pc
-        else if (d == this.radius) {
-            Vector_unit_t normale = pc - this.center;
+        else if (d == this->radius) {
+            Vector_t normale = pc - this->centre;
             Intersection_t i(distance, normale, this);
             return i;
         }
         // if d < r then 2 intersections, find the closest and its position
         else {
-            double c = sqrt(this.radius**2 - d**2); // pythagorean theorem
+            double c = sqrt(pow(this->radius, 2) - pow(d, 2)); // pythagorean theorem
             double di1 = distance - c;
 
-            Vector_t i1 = ray.origin + di1*ray.direction;
-            Vector_unit_t normale = i1 - this.center;
+            Vector_t i1 = ray.origin + ray.direction * di1;
+            Vector_t normale = i1 - this->centre;
             Intersection_t i(di1, normale, this);
             return i;
         }
